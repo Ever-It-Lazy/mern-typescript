fdb00ecd4a2271cbd38b70f7899775964a0264e6
Everett Lindsay <everettsjunk@gmail.com>
2023-01-05 19:11:39 -0500

    working on showing the decks on the landing page

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import { config } from "dotenv";
+import cors from "cors";
 import Deck from "./models/Deck";
 
 mongoose.set('strictQuery', false);
@@ -9,7 +10,13 @@ const app = express();
 
 const PORT = 5000;
 
+app.use(cors());
 app.use(express.json());
+
+app.get('/decks', async (req: Request, res: Response) => {
+	const decks = await Deck.find();
+	res.json(decks);
+});
 
 app.post('/decks', async (req: Request, res: Response) => {
 	console.log(req.body)
