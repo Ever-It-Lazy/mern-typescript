cd09af9eab6fdeb8351cf458d14d48af8e819f83
Everett Lindsay <everettsjunk@gmail.com>
2023-01-07 21:16:38 -0500

    controller refactoring

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,9 @@ import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import { config } from "dotenv";
 import cors from "cors";
-import Deck from "./models/Deck";
+import { createDeckController } from "./controllers/createDeckController";
+import { deleteDeckController } from "./controllers/deleteDeckController";
+import { getDecksController } from "./controllers/getDecksController";
 
 mongoose.set('strictQuery', false);
 config();
@@ -13,28 +15,11 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/decks', async (req: Request, res: Response) => {
-	const decks = await Deck.find();
-	res.json(decks);
-});
+app.get('/decks', getDecksController);
 
-app.post('/decks', async (req: Request, res: Response) => {
-	const newDeck = new Deck({
-		title: req.body.title
-	});
-	const createdDeck = await newDeck.save();
-	res.json(createdDeck);
-});
+app.post('/decks', createDeckController);
 
-app.delete('/decks/:deckId', async (req: Request, res: Response) => {
-	const deckId = req.params.deckId;
-
-	const deck = await Deck.findByIdAndDelete(deckId);
-
-	res.json({
-		message: "successfully deleted the entry"
-	});
-});
+app.delete('/decks/:deckId', deleteDeckController);
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
 	console.log(`listening on port ${PORT}`);
